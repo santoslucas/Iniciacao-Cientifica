5c809721f35b57dd9726be0d263e237dfbc63c11
Lucas Santos Rodrigues <84406944+santoslucas@users.noreply.github.com>
2022-10-11 19:27:42 -0300

    Delete ga_test.py

--- ga_test.py
+++
@@ -1,9 +0,0 @@
-from pymoo.algorithms.soo.nonconvex.ga import GA
-import pymoo.problems.single as f_obj
-from pymoo.optimize import minimize
-
-
-def teste():
-    print("isso eh um teste")
-
-print(ga(100, 1))
